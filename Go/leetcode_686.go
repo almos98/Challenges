864cde5f1a2f770af4de801e47d9e165ad58b4fb
Alexion Ramos <ramosa@tcd.ie>
2019-10-29 16:43:24 +0000

    Solved Leetcode Problem 686.

---
+++ Go/leetcode_686.go
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode id=686 lang=golang
+ *
+ * [686] Repeated String Match
+ *
+ * https://leetcode.com/problems/repeated-string-match/description/
+ *
+ * algorithms
+ * Easy (31.79%)
+ * Total Accepted:    77.2K
+ * Total Submissions: 242.7K
+ * Testcase Example:  '"abcd"\n"cdabcdab"'
+ *
+ * Given two strings A and B, find the minimum number of times A has to be
+ * repeated such that B is a substring of it. If no such solution, return -1.
+ * 
+ * For example, with A = "abcd" and B = "cdabcdab".
+ * 
+ * Return 3, because by repeating A three times (“abcdabcdabcd”), B is a
+ * substring of it; and B is not a substring of A repeated two times
+ * ("abcdabcd").
+ * 
+ * Note:
+ * The length of A and B will be between 1 and 10000.
+ * 
+ */
+import (
+    "strings"
+)
+
+func repeatedStringMatch(A string, B string) int {
+    n := (len(B) - 1) / len(A) + 1
+    str := strings.Repeat(A, n)
+    if strings.Count(str, B) > 0 {
+        return n
+    }
+    str = strings.Repeat(A, n+1)
+    if strings.Count(str, B) > 0 {
+        return n + 1
+    }
+    return -1
+}
